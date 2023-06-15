04bbd232d0fdd4ef9dea5f3535be1ffccab59b6d
Liber Wang <yili.wang@longbridge.sg>
2023-06-15 20:48:15 +0800

    support current environment

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,12 @@ pub trait Environment {
     fn default_environment_detector() -> &'static str {
         "APP_ENVIRONMENT"
     }
+
+    fn current_environment() -> &'static str {
+        let s = std::env::var(Self::default_environment_detector())
+            .unwrap_or(Self::default_environment().to_lowercase());
+        Box::leak(s.into_boxed_str())
+    }
 }
 
 pub fn parse_configuration<'a, T: Environment>() -> Result<T, config::ConfigError>
@@ -28,21 +34,14 @@ where
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join(T::default_configuration_dir());
 
-    let environment = std::env::var(T::default_environment_detector())
-        .unwrap_or(T::default_environment().to_lowercase());
+    let environment = T::current_environment();
 
-    log::info!(
-        "{} = {}",
-        T::default_environment_detector(),
-        environment.as_str()
-    );
+    log::info!("{} = {}", T::default_environment_detector(), environment);
     let settings = config::Config::builder()
         // Read the "default" configuration file
         .add_source(config::File::from(configuration_directory.join("base")).required(true))
         // Layer on the environment-specific values.
-        .add_source(
-            config::File::from(configuration_directory.join(environment.as_str())).required(true),
-        )
+        .add_source(config::File::from(configuration_directory.join(environment)).required(true))
         // Add in settings from environment variables (with a prefix of APP and '__' as separator)
         // E.g. `APP__APPLICATION__PORT=5001 would set `Settings.application.port`
         .add_source(
